9814c83bf77379f2d5289721128a28c6d9dc4f7a
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2024-07-22 13:05:29 +0200

    Debug Azure Function not working.

--- function_app.py
+++ function_app.py
@@ -4,8 +4,8 @@ from ytsum import say_hello
 app = func.FunctionApp()
 
 
-@app.function_name(name="hello")
-@app.route(route="main/hello", auth_level=func.AuthLevel.ANONYMOUS)
+@app.function_name(name="HttpTrigger1")
+@app.route(route="hello")
 def main(req: func.HttpRequest) -> func.HttpResponse:
     output = say_hello()
     return func.HttpResponse(body=output, status_code=200)
