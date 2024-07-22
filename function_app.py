442d0a3f54502c40fa0e893bb29aee2960e21862
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2024-07-22 14:32:22 +0200

    Simplify function app code.

--- function_app.py
+++ function_app.py
@@ -1,5 +1,6 @@
 import azure.functions as func
-from ytsum import say_hello
+
+# from ytsum import say_hello
 
 app = func.FunctionApp()
 
@@ -7,5 +8,5 @@ app = func.FunctionApp()
 @app.function_name(name="HttpTrigger1")
 @app.route(route="hello")
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    output = say_hello()
+    output = "No calls to ytsum package."
     return func.HttpResponse(body=output, status_code=200)
