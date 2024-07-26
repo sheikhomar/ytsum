d18ce7e9affab64ed4f220855cea834641783b6c
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2024-07-26 16:49:21 +0200

    Refactor.

--- src/ytsum/scene_detection/adaptive.py
+++ src/ytsum/scene_detection/adaptive.py
@@ -1,32 +1,9 @@
 from pathlib import Path
 from time import time
-from typing import List, Tuple
 
 from scenedetect import AdaptiveDetector, FrameTimecode, VideoStream, detect, open_video
 from ytsum.scene_detection.common import SceneDetectionResult, SceneDetector, SceneInfo
-
-
-def format_elapsed_time(start_time: float, end_time: float) -> str:
-    elapsed_seconds: float = end_time - start_time
-    minutes_seconds: Tuple[int, float] = divmod(elapsed_seconds, 60)
-    minutes: int = int(minutes_seconds[0])
-    seconds: int = int(minutes_seconds[1])
-    milliseconds: int = int((elapsed_seconds - int(elapsed_seconds)) * 1000)
-
-    parts: List[str] = []
-    if minutes > 0:
-        parts.append(f"{minutes} minute{'s' if minutes != 1 else ''}")
-    if seconds > 0:
-        parts.append(f"{seconds} second{'s' if seconds != 1 else ''}")
-    if milliseconds > 0:
-        parts.append(f"{milliseconds} millisecond{'s' if milliseconds != 1 else ''}")
-
-    if len(parts) > 1:
-        return f"{', '.join(parts[:-1])} and {parts[-1]}"
-    elif len(parts) == 1:
-        return parts[0]
-    else:
-        return "0 milliseconds"
+from ytsum.utils import format_elapsed_time
 
 
 class AdaptiveSceneDetector(SceneDetector):
--- src/ytsum/utils.py
+++ src/ytsum/utils.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Iterable, List, Tuple
 
 
 def convert_timestamp_to_ms(timestamp: str) -> int:
@@ -20,3 +20,26 @@ def batched(iterable: Iterable, n: int = 1) -> Iterable:
     l = len(iterable)
     for ndx in range(0, l, n):
         yield iterable[ndx : min(ndx + n, l)]
+
+
+def format_elapsed_time(start_time: float, end_time: float) -> str:
+    elapsed_seconds: float = end_time - start_time
+    minutes_seconds: Tuple[int, float] = divmod(elapsed_seconds, 60)
+    minutes: int = int(minutes_seconds[0])
+    seconds: int = int(minutes_seconds[1])
+    milliseconds: int = int((elapsed_seconds - int(elapsed_seconds)) * 1000)
+
+    parts: List[str] = []
+    if minutes > 0:
+        parts.append(f"{minutes} minute{'s' if minutes != 1 else ''}")
+    if seconds > 0:
+        parts.append(f"{seconds} second{'s' if seconds != 1 else ''}")
+    if milliseconds > 0:
+        parts.append(f"{milliseconds} millisecond{'s' if milliseconds != 1 else ''}")
+
+    if len(parts) > 1:
+        return f"{', '.join(parts[:-1])} and {parts[-1]}"
+    elif len(parts) == 1:
+        return parts[0]
+    else:
+        return "0 milliseconds"
