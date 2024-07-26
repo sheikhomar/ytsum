26de2ec72781d86af80dcfd0a4c3b2d421ab48ed
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2024-07-26 13:40:38 +0200

    Refactor.

--- tools/detect_scenes.py
+++ tools/detect_scenes.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from ytsum.scene_detection.adaptive import AdaptiveSceneDetector
+from ytsum.scene_detection.common import SceneDetectionResult
 from ytsum.scene_detection.eval import SceneDetectionEvaluator, VideoSceneAnnotation
 
 
@@ -10,25 +11,37 @@ def run_and_evaluate_adaptive_detector(
     min_scene_length_secs: float = 1,
     min_content_value: int = 5,
 ) -> None:
-    video_file_paths = list(video_dir.glob("*.mp4"))
-    if len(video_file_paths) != 1:
-        print(
-            f"Expected 1 video file in {video_dir}. But found {len(video_file_paths)} files."
-        )
-        return
-
-    video_file_path = video_file_paths[0]
-
-    detector = AdaptiveSceneDetector(
-        adaptive_threshold=threshold,
-        min_scene_length_secs=min_scene_length_secs,
-        min_content_value=min_content_value,
+    detector_result_file_path = (
+        video_dir
+        / f"scene-detection-result-threshold{threshold:0.4f}-minlen{min_scene_length_secs:0.4f}-mincontent{min_content_value}.json"
     )
-    result = detector.run(video_file_path=video_file_path)
+
+    if not detector_result_file_path.exists():
+        video_file_paths = list(video_dir.glob("*.mp4"))
+        if len(video_file_paths) != 1:
+            print(
+                f"Expected 1 video file in {video_dir}. But found {len(video_file_paths)} files."
+            )
+            return
+
+        video_file_path = video_file_paths[0]
+
+        detector = AdaptiveSceneDetector(
+            adaptive_threshold=threshold,
+            min_scene_length_secs=min_scene_length_secs,
+            min_content_value=min_content_value,
+        )
+        result = detector.run(video_file_path=video_file_path)
+
+        detector_result_file_path.write_text(result.model_dump_json(indent=2))
+    else:
+        result = SceneDetectionResult.model_validate_json(
+            json_data=detector_result_file_path.read_text()
+        )
 
     annotation_file_path = video_dir / "scene-annotation.json"
     if annotation_file_path.exists():
-        print(f"Evaluating {video_file_path} with {annotation_file_path}...")
+        print(f"Evaluating {result.video_file_path} with {annotation_file_path}...")
         annotation = VideoSceneAnnotation.model_validate_json(
             json_data=annotation_file_path.read_text()
         )
