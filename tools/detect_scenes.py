dd8c1de581937513fba454d1d080845887e4648b
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2024-07-26 16:04:57 +0200

    Evaluate more parameters.

--- tools/detect_scenes.py
+++ tools/detect_scenes.py
@@ -53,16 +53,42 @@ def run_and_evaluate_adaptive_detector(
         print(evaluation_result.model_dump_json(indent=2))
 
 
+def run_eval_only(video_dir: Path) -> None:
+    annotation_file_path = video_dir / "scene-annotation.json"
+    if annotation_file_path.exists():
+        result_file_paths = video_dir.glob("scene-detection-result-*.json")
+        for result_file_path in result_file_paths:
+            result = SceneDetectionResult.model_validate_json(
+                json_data=result_file_path.read_text()
+            )
+
+            print(f"Evaluating {result.video_file_path} with {annotation_file_path}...")
+            annotation = VideoSceneAnnotation.model_validate_json(
+                json_data=annotation_file_path.read_text()
+            )
+
+            evaluator = SceneDetectionEvaluator(tolerance_secs=1.5)
+
+            evaluation_result = evaluator.run(annotation=annotation, result=result)
+
+            print(evaluation_result.model_dump_json(indent=2))
+
+
 def main() -> None:
     video_dir = Path("data/downloads")
     video_ids = ["Onf1UqKPMR4", "MBdEWLqfdms", "4gcGkFAG7OA"]
-    for video_id in video_ids:
-        run_and_evaluate_adaptive_detector(
-            video_dir=video_dir / video_id,
-            threshold=2.5,
-            min_scene_length_secs=1,
-            min_content_value=5,
-        )
+
+    run_eval_only(video_dir=video_dir / "4gcGkFAG7OA")
+
+    for content_val in [1, 3, 5, 7, 9, 10]:
+        for threshold in [0.5, 1.0, 2.0, 2.5, 3.0, 3.5]:
+            for video_id in video_ids:
+                run_and_evaluate_adaptive_detector(
+                    video_dir=video_dir / video_id,
+                    threshold=threshold,
+                    min_scene_length_secs=1,
+                    min_content_value=content_val,
+                )
 
 
 if __name__ == "__main__":
